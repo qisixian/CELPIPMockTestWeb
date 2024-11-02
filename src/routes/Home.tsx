2e8165fed881813f7241e6f122af88d26bf0f290
Sixian <2235127438@qq.com>
2024-11-02 15:41:23 -0700

    rename project

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -8,6 +8,7 @@ import { Button, notification } from 'antd';
 import {Routes, Route} from "react-router-dom";
 import MyCard from "../components/MyCard";
 import MyImage from '../components/MyImage';
+import axios from 'axios';
 
 
 
@@ -31,9 +32,21 @@ function Home() {
         });
     };
 
+
+    const googleLogin = () => {
+        axios.get("http://localhost:8080/oauth2/authorization/google", {
+            // headers: {
+            //     'Access-Control-Allow-Origin': '*'
+            // }
+        })
+            .then(function (response) {
+                console.log(response.data);
+            });
+    };
     return (
         <>
             <h1>CELPIP Test</h1>
+            <Button type="primary" onClick={() => googleLogin()}>Authentication</Button>
             <MyImage imageUrl="http://localhost:8080/media/2"/>
             <MyList items={data}>1</MyList>
             {contextHolder}
